a29091812ad9b0ff77bfb5e466e07344b16db8c1
summereth <liq.qian.l@gmail.com>
2024-03-05 14:56:56 -0800

    07-usepopcorn: add loading state & handle errors

--- 07-usepopcorn/usepopcorn/src/App.js
+++ 07-usepopcorn/usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { tempMovieData, tempWatchedData } from "./data";
 import ExpandableBox from "./components/ExpandableBox";
 import NavBar from "./components/NavBar";
@@ -6,21 +6,40 @@ import NavSearchResult from "./components/NavSearchResult";
 import SearchMovieItems from "./components/SearchMovieItems";
 import WatchedMovieItems from "./components/WatchedMovieItems";
 import WatchedMovieSummary from "./components/WatchedMovieSummary";
-import { useEffect } from "react";
+import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
-  const query = "interstellar";
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const query = "456desacf";
 
   useEffect(() => {
+    setIsLoading(true);
     // async function must be called inside
     const fetchMovies = async () => {
-      const res = await fetch(
-        `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${query}`
-      );
-      const data = await res.json();
-      setMovies(data.Search);
+      try {
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${query}`
+        );
+        if (!res.ok) {
+          throw new Error(
+            "Something went wrong when trying to fetch movie data"
+          );
+        }
+
+        const data = await res.json();
+        if (data.Response === "False") {
+          throw new Error(data.Error);
+        }
+        setMovies(data.Search);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
     };
     fetchMovies();
   }, []);
@@ -32,7 +51,9 @@ export default function App() {
       </NavBar>
       <main className="main">
         <ExpandableBox>
-          <SearchMovieItems movies={movies} />
+          {isLoading && <Loader />}
+          {error && <ErrorMessage message={error} />}
+          {!isLoading && !error && <SearchMovieItems movies={movies} />}
         </ExpandableBox>
         <ExpandableBox>
           <WatchedMovieSummary watched={watched} />
---
+++ 07-usepopcorn/usepopcorn/src/components/ErrorMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorMessage = ({ message }) => {
+  return (
+    <p className="error">
+      <span>❌</span> {message}
+    </p>
+  );
+};
+
+export default ErrorMessage;
