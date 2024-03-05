38acf05fea764a8eab201bc586e4720d86e2c825
summereth <liq.qian.l@gmail.com>
2024-03-05 14:27:01 -0800

    07-usepopcorn: fetch data from API

--- 07-usepopcorn/usepopcorn/src/App.js
+++ 07-usepopcorn/usepopcorn/src/App.js
@@ -6,10 +6,24 @@ import NavSearchResult from "./components/NavSearchResult";
 import SearchMovieItems from "./components/SearchMovieItems";
 import WatchedMovieItems from "./components/WatchedMovieItems";
 import WatchedMovieSummary from "./components/WatchedMovieSummary";
+import { useEffect } from "react";
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+  const query = "interstellar";
+
+  useEffect(() => {
+    // async function must be called inside
+    const fetchMovies = async () => {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${query}`
+      );
+      const data = await res.json();
+      setMovies(data.Search);
+    };
+    fetchMovies();
+  }, []);
 
   return (
     <>
--- 07-usepopcorn/usepopcorn/src/components/WatchedMovieSummary.jsx
+++ 07-usepopcorn/usepopcorn/src/components/WatchedMovieSummary.jsx
@@ -1,7 +1,7 @@
-export default function WatchedMovieSummary({ watched }) {
-  const average = (arr) =>
-    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+const average = (arr) =>
+  arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+export default function WatchedMovieSummary({ watched }) {
   const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
   const avgUserRating = average(watched.map((movie) => movie.userRating));
   const avgRuntime = average(watched.map((movie) => movie.runtime));
