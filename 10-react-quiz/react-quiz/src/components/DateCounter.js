b8723888cb611a5b56ea90276602614a500419dd
summereth <liq.qian.l@gmail.com>
2024-03-08 14:13:07 -0800

    10-react-quiz: refactor all states in DateCounter by the use of useReducer

--- 10-react-quiz/react-quiz/src/components/DateCounter.js
+++ 10-react-quiz/react-quiz/src/components/DateCounter.js
@@ -1,40 +1,51 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 
+const initialState = { step: 1, count: 0 };
 function reducer(state, action) {
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setValue") return action.payLoad;
+  switch (action.type) {
+    case "incCount":
+      return { ...state, count: state.count + state.step };
+    case "decCount":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payLoad };
+    case "setStep":
+      return { ...state, step: action.payLoad };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("Unknown action in reducer");
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { step, count } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch({ type: "dec" });
+    dispatch({ type: "decCount" });
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    dispatch({ type: "inc" });
+    dispatch({ type: "incCount" });
   };
 
   const defineCount = function (e) {
-    dispatch({ type: "setValue", payLoad: Number(e.target.value) });
+    dispatch({ type: "setCount", payLoad: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payLoad: Number(e.target.value) });
   };
 
   const reset = function () {
-    dispatch({ type: "setValue", payLoad: 0 });
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
