46e16c83ba29b63a9d016b5d28cdc2e7bd3fde4d
summereth <liq.qian.l@gmail.com>
2024-03-08 15:24:47 -0800

    10-react-quiz: fetch data from fake API(npm pck json-server) and update state with useReducer

--- 10-react-quiz/react-quiz/src/App.js
+++ 10-react-quiz/react-quiz/src/App.js
@@ -1,12 +1,45 @@
 import React from "react";
-import DateCounter from "./components/DateCounter.js";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { useEffect, useReducer } from "react";
 
-const App = () => {
+const initialState = {
+  questions: [],
+  // status: loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataDownloaded":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown action in reducer");
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // fetch data and update state with dispatch
+  useEffect(() => {
+    fetch("http://localhost:3030/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataDownloaded", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div>
-      <DateCounter />
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
-};
+}
 
 export default App;
