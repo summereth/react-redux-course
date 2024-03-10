043d1dad569b43f2324536cef3b168756b12834e
summereth <liq.qian.l@gmail.com>
2024-03-09 20:28:18 -0800

    10-react-quiz: handling user's answer

--- 10-react-quiz/react-quiz/src/App.js
+++ 10-react-quiz/react-quiz/src/App.js
@@ -12,6 +12,8 @@ const initialState = {
   // status: loading, error, ready, active, finished
   status: "loading",
   index: 0,
+  answer: null, // user's answer to current question (questions[index])
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -32,10 +34,21 @@ function reducer(state, action) {
         ...state,
         status: "active",
       };
+    case "newAnswer":
+      const question = state.questions[state.index];
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          state.points +
+          (action.payload === question.correctOption ? question.points : 0),
+      };
     case "nextQuestion":
       return {
         ...state,
         index: state.index + 1,
+        answer: null,
       };
     default:
       throw new Error("Unknown action in reducer");
@@ -44,7 +57,7 @@ function reducer(state, action) {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { status, questions, index } = state;
+  const { status, questions, index, answer } = state;
 
   // fetch data and update state with dispatch
   useEffect(() => {
@@ -69,6 +82,10 @@ function App() {
         {status === "active" && (
           <Question
             question={questions[index]}
+            answer={answer}
+            onAnswer={(answer) =>
+              dispatch({ type: "newAnswer", payload: answer })
+            }
             clickNext={() => dispatch({ type: "nextQuestion" })}
           />
         )}
--- 10-react-quiz/react-quiz/src/components/Question.jsx
+++ 10-react-quiz/react-quiz/src/components/Question.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-function Question({ question, clickNext }) {
+function Question({ question, answer, onAnswer, clickNext }) {
   return (
     <div>
       <h4>{question.question}</h4>
       <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
+        {question.options.map((option, i) => (
+          <button
+            className={`btn btn-option ${i === answer ? "answer" : ""} ${
+              answer !== null && i === question.correctOption
+                ? "correct"
+                : answer
+                ? "wrong"
+                : ""
+            }`}
+            key={option}
+            disabled={answer !== null}
+            onClick={() => onAnswer(i)}
+          >
             {option}
           </button>
         ))}
