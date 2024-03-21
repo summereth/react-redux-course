af5ae6eeff89eead51dea31103977247a406d436
summereth <liq.qian.l@gmail.com>
2024-03-20 17:23:46 -0700

    11-worldwise: add Link

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -3,11 +3,13 @@ import Homepage from "../pages/Homepage";
 import ProductPage from "../pages/ProductPage";
 import PricingPage from "../pages/PricingPage";
 import PageNotFound from "../pages/PageNotFound";
+import PageNav from "../components/PageNav";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <PageNav />
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<ProductPage />} />
