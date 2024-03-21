e64db6ab269dd492eeb2da3bcd6f354e92fc235c
summereth <liq.qian.l@gmail.com>
2024-03-21 16:30:49 -0700

    11-worldwise: build CityList - fake API, fetch data, display

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,12 +1,34 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Homepage from "../pages/Homepage";
 import ProductPage from "../pages/ProductPage";
 import PricingPage from "../pages/PricingPage";
 import PageNotFound from "../pages/PageNotFound";
 import AppLayout from "../pages/AppLayout";
 import LoginPage from "../pages/LoginPage";
+import CityList from "../components/CityList";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch("http://localhost:8000/cities");
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
@@ -16,8 +38,14 @@ function App() {
           <Route path="pricing" element={<PricingPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>List of cities</p>} />
-            <Route path="cities" element={<p>List of cities</p>} />
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            />
             <Route path="countries" element={<p>List of countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
