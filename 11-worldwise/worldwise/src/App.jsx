951cc4aa22039b0c99422a4f207284a5e6f9b140
summereth <liq.qian.l@gmail.com>
2024-03-20 17:05:56 -0700

    11-worldwise: add routes

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "../pages/Homepage";
+import ProductPage from "../pages/ProductPage";
+import PricingPage from "../pages/PricingPage";
+import PageNotFound from "../pages/PageNotFound";
+
 function App() {
-  return <div>Worldwise</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<ProductPage />} />
+          <Route path="pricing" element={<PricingPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
