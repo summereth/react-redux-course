b6650ba54ff78cf4905ffee63f15a12539cbb0b3
summereth <liq.qian.l@gmail.com>
2024-03-21 20:59:16 -0700

    11-worldwise: Add form and navigate to form - useNavigate, set redirect on /app - <Navigate />

--- 11-worldwise/worldwise/components/Map.jsx
+++ 11-worldwise/worldwise/components/Map.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Map.module.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function Map() {
+  const navigate = useNavigate();
   const [query, setQuery] = useSearchParams();
   const lat = query.get("lat");
   const lng = query.get("lng");
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <h1>Map</h1>
       <h1>
         Position: {lat}, {lng}
--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Homepage from "../pages/Homepage";
 import ProductPage from "../pages/ProductPage";
@@ -9,6 +9,7 @@ import LoginPage from "../pages/LoginPage";
 import CityList from "../components/CityList";
 import CountryList from "../components/CountryList";
 import City from "../components/City";
+import Form from "../components/Form";
 
 function App() {
   const [cities, setCities] = useState([]);
@@ -40,10 +41,7 @@ function App() {
           <Route path="pricing" element={<PricingPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="app" element={<AppLayout />}>
-            <Route
-              index
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route
               path="cities"
               element={<CityList cities={cities} isLoading={isLoading} />}
@@ -53,7 +51,7 @@ function App() {
               path="countries"
               element={<CountryList cities={cities} isLoading={isLoading} />}
             />
-            <Route path="form" element={<p>Form</p>} />
+            <Route path="form" element={<Form />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
