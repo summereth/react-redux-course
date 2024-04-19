24e128c7dfa1a1701d94bf80348be0b625d7ef5f
summereth <liq.qian.l@gmail.com>
2024-04-18 18:35:59 -0700

    15-redux-intro: refactor store.js with redux toolkit

--- 15-redux-intro/redux-intro/src/index.js
+++ 15-redux-intro/redux-intro/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import store from "./store";
+import store from "./store-classic_redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
